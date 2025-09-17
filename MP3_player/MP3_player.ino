efd9d1e930d3e756de10586c186582f6a3892812
kaydev6 <kayleb.uni.go@gmail.com>
2025-09-18 01:17:37 +0200

    DFPlayer problems

--- MP3_player/MP3_player.ino
+++ MP3_player/MP3_player.ino
@@ -1,3 +1,9 @@
+#include <HardwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+HardwareSerial mySerial(1);
+DFRobotDFPlayerMini DFPlayer;
+
 #define playButtonPin 25
 #define skipButtonPin 33 
 #define prevButtonPin 26
@@ -8,11 +14,21 @@ int prevButtonState = 0;
 
 void setup() {
   Serial.begin(115200);
+  delay(500);
+
+  mySerial.begin(115200, SERIAL_8N1, 16, 17);
 
   pinMode(playButtonPin, INPUT_PULLUP);
   pinMode(skipButtonPin, INPUT_PULLUP);
   pinMode(prevButtonPin, INPUT_PULLUP);
 
+   if (!DFPlayer.begin(mySerial)) {
+    Serial.println("DFPlayer not detected!");
+    while (true);
+  }
+
+  DFPlayer.loopFolder(01);
+  DFPlayer.volume(5);
   delay(2000);
 }
 
